80816c2e73749d1df5edd1a3cda3aac5496aae54
K G Ashalatha <kgashalatha3@gmail.com>
2026-04-24 11:10:26 +0530

    Add files via upload

---
+++ Rotatearrayright.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Rotatearrayright
+{
+public static void main (String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int[] arr=new int[n];
+for (int  i=0;i<n;i++)
+{
+arr[i]=sc.nextInt();
+}
+int k=sc.nextInt();
+k=k%n;
+reverse(arr ,0,n-1);
+reverse(arr,0,k-1);
+reverse(arr,k,n-1);
+for(int i=0;i<n;i++)
+{
+System.out.println(arr[i]+"  ");
+}
+}
+static void reverse(int[] arr,int start,int end)
+{
+while(start<end)
+{
+int temp=arr[start];
+arr[start]=arr[end];
+arr[end]=temp;
+start++;
+end--;
+}
+}
+}
